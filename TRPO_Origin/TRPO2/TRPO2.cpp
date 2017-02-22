07edcd5a34007d394fb1775d4bd72f4148b03a76
Veltare <ya.pcaristokrat@yandex.ru>
2017-02-22 09:38:22 +0700

    Eto laboratornay po TRPO 2.1

--- TRPO_Origin/TRPO2/TRPO2.cpp
+++ TRPO_Origin/TRPO2/TRPO2.cpp
@@ -6,7 +6,6 @@
 #include <clocale>
 
 float raschet(float vklad, float time_vk);
-void inspection(int value);
 using namespace std;
 int main()
 {
@@ -40,9 +39,11 @@ int main()
 		}
 		cin.clear();
 		cout << "Непрпвильный ввод данных " << endl;
-		cin.ignore(10, '\n');
-	}
-	cout << endl << raschet(vklad, time_vk) << endl;
+		cin.ignore(10, '\n');	}
+	cout << "Ваш вклад:"<< vklad << endl;
+	cout << "Время:" << time_vk << endl;
+	cout << "Итого:" << raschet(vklad, time_vk) << endl;
+	cout << endl <<"Ваш доход: "<<vklad-raschet(vklad, time_vk) << endl;
 	system("pause");
 	return 0;
 }
@@ -53,6 +54,7 @@ float raschet(float vklad, float time_vk)
 
 	if (vklad < 100000)
 	{
+		//Если вклад меньше чем 100000.
 
 		if ((timevknew <= 365) && (timevknew >= 241))
 		{
@@ -62,24 +64,21 @@ float raschet(float vklad, float time_vk)
 		if ((timevknew <= 240) && (timevknew >= 121))
 		{
 			vkladnew = vkladnew + (vkladnew *((vkladnew * 0, 06)*timevknew)) / 365 / 100;
-			cout << vkladnew;
 			return vkladnew;
 		}
 		if ((timevknew <= 120) && (timevknew >= 31))
 		{
 			vkladnew = vkladnew + (vkladnew *((vkladnew * 0, 03)*timevknew)) / 365 / 100;
-			cout << vkladnew;
 			return vkladnew;
 		}
 		if ((timevknew <= 30) && (timevknew > 0))
 		{
 			vkladnew = vkladnew - (vkladnew *((vkladnew * 0, 10)*timevknew)) / 365 / 100;
-			cout << vkladnew;
 			return vkladnew;
 		}
 	}
 	{
-
+		//Если вклад больше 100000.
 		if ((timevknew <= 365) && (timevknew >= 241))
 		{
 			vkladnew = vkladnew + (vkladnew *((vkladnew * 0, 15)*timevknew)) / 365 / 100;
@@ -88,19 +87,16 @@ float raschet(float vklad, float time_vk)
 		if ((timevknew <= 240) && (timevknew >= 121))
 		{
 			vkladnew = vkladnew + (vkladnew *((vkladnew * 0.08)*timevknew)) / 365 / 100;
-			cout << vkladnew;
 			return vkladnew;
 		}
 		if ((timevknew <= 120) && (timevknew >= 31))
 		{
 			vkladnew = vkladnew + (vkladnew *((vkladnew * 0, 03)*timevknew)) / 365 / 100;
-			cout << vkladnew;
 			return vkladnew;
 		}
 		if ((timevknew <= 30) && (timevknew > 0))
 		{
 			vkladnew = vkladnew - (vkladnew *((vkladnew * 0, 10)*timevknew)) / 365 / 100;
-			cout << vkladnew;
 			return vkladnew;
 
 
